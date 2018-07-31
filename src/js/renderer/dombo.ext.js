df2bf0f47a605541adcef512398b523ff4ffc9af
Josue Mavarez <josuemavarez@gmail.com>
2018-07-31 14:54:15 -0300

    Update Dombo ext

--- src/js/renderer/dombo.ext.js
+++ src/js/renderer/dombo.ext.js
@@ -8,14 +8,4 @@ module.exports = function (dombo) {
       node.textContent = str;
     }, true);
   };
-
-  dombo.fn.toggleClass = function (name, state) {
-    return this.forEach(function (node) {
-      node = dombo(node)
-      if (state === true) return node.addClass(name)
-      if (state === false) return node.removeClass(name)
-      if (node.hasClass(name)) return node.removeClass(name)
-      return node.addClass(name)
-    });
-  }
 };
